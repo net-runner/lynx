1d871ad75781f0df43b0e2c3ddc4451e9083e70b
net-runner <slazynski.j@gmail.com>
2022-05-06 22:25:48 +0200

    Next init.

---
+++ packages/linx-next/jest.config.ts
@@ -0,0 +1,10 @@
+module.exports = {
+  displayName: 'linx-next',
+  preset: '../../jest.preset.ts',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/next/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/packages/linx-next',
+};
