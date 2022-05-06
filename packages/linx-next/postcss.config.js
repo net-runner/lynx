445251f7d9f5d17c7396671582d242165176e4e0
net-runner <slazynski.j@gmail.com>
2022-05-06 22:52:39 +0200

    Added tailwind for next.

---
+++ packages/linx-next/postcss.config.js
@@ -0,0 +1,18 @@
+const { join } = require('path');
+
+
+module.exports = {
+
+  plugins: {
+
+    tailwindcss: {
+
+      config: join(__dirname, 'tailwind.config.js'),
+
+    },
+
+    autoprefixer: {},
+
+  },
+
+};
---
+++ packages/linx-next/tailwind.config.js
@@ -0,0 +1,24 @@
+const { createGlobPatternsForDependencies } = require('@nrwl/react/tailwind');
+
+const { join } = require('path');
+
+
+module.exports = {
+
+  content: [
+
+    join(__dirname, 'src/**/*!(*.stories|*.spec).{ts,tsx,html}'),
+
+    ...createGlobPatternsForDependencies(__dirname),
+
+  ],
+
+  theme: {
+
+    extend: {},
+
+  },
+
+  plugins: [],
+
+};
