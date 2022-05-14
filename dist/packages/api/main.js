d5d7be48d2e1b4a95e5d42312eb1cf2b515efc81
net-runner <slazynski.j@gmail.com>
2022-05-14 10:22:06 +0200

    Rolled back hyper-express version

--- dist/packages/api/main.js
+++ dist/packages/api/main.js
@@ -1,2 +1,73 @@
-(()=>{"use strict";var __webpack_modules__={142:module=>{module.exports=require("dotenv")},448:module=>{module.exports=require("hyper-express")}},__webpack_module_cache__={};function __webpack_require__(moduleId){var cachedModule=__webpack_module_cache__[moduleId];if(void 0!==cachedModule)return cachedModule.exports;var module=__webpack_module_cache__[moduleId]={exports:{}};return __webpack_modules__[moduleId](module,module.exports,__webpack_require__),module.exports}var __webpack_exports__={};(()=>{var exports=__webpack_exports__;Object.defineProperty(exports,"__esModule",{value:!0});const hyper_express_1=__webpack_require__(448);__webpack_require__(142);const webserver=new hyper_express_1.Server;webserver.get("/hello",((request,response)=>{response.send("Hello World")}));const port=process.env.PORT||80;webserver.listen(port).then((socket=>console.log("Webserver started on port: "+port))).catch((error=>console.log("Failed to start webserver on port: "+port)))})();var __webpack_export_target__=exports;for(var i in __webpack_exports__)__webpack_export_target__[i]=__webpack_exports__[i];__webpack_exports__.__esModule&&Object.defineProperty(__webpack_export_target__,"__esModule",{value:!0})})();
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "dotenv":
+/***/ ((module) => {
+
+module.exports = require("dotenv");
+
+/***/ }),
+
+/***/ "hyper-express":
+/***/ ((module) => {
+
+module.exports = require("hyper-express");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
+var exports = __webpack_exports__;
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+const hyper_express_1 = __webpack_require__("hyper-express");
+__webpack_require__("dotenv");
+const webserver = new hyper_express_1.Server();
+// Create GET route to serve 'Hello World'
+webserver.get('/hello', (request, response) => {
+    response.send('Hello World');
+});
+// Activate webserver by calling .listen(port, callback);
+const port = process.env.PORT || 80;
+webserver
+    .listen(port)
+    .then((socket) => console.log('Webserver started on port: ' + port))
+    .catch((error) => console.log('Failed to start webserver on port: ' + port));
+
+})();
+
+var __webpack_export_target__ = exports;
+for(var i in __webpack_exports__) __webpack_export_target__[i] = __webpack_exports__[i];
+if(__webpack_exports__.__esModule) Object.defineProperty(__webpack_export_target__, "__esModule", { value: true });
+/******/ })()
+;
 //# sourceMappingURL=main.js.map
