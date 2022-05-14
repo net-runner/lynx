f939f05cb540a71b49b0cff4d82ac2b82de817ec
Przemek <przemek-szczepaniak@tlen.pl>
2022-05-14 19:00:01 +0200

    updated basic folder structure

--- packages/linx-next/pages/_app.tsx
+++ packages/linx-next/pages/_app.tsx
@@ -1,6 +1,6 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
-import './styles.css';
+import '../styles/app.scss';
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
