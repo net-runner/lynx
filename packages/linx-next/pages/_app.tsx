65886616ea2c7222538762ebc3f778c4afb5e548
net-runner <slazynski.j@gmail.com>
2022-05-15 08:47:30 +0200

    Pwa first step.

--- packages/linx-next/pages/_app.tsx
+++ packages/linx-next/pages/_app.tsx
@@ -6,7 +6,7 @@ function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Welcome to linx-next!</title>
+        <title>Lynx - link sharing platform</title>
       </Head>
       <main className="app">
         <Component {...pageProps} />
