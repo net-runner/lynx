e83d515f478fdefbd9d22ab741673a768e563e94
net-runner <slazynski.j@gmail.com>
2022-05-15 12:53:50 +0200

    Added more default pages.

---
+++ packages/linx-next/pages/_offline.tsx
@@ -0,0 +1,5 @@
+//Static page for handling app being offline or as a fallback for non WebWorker cached routes
+
+export default function Offline() {
+  return <h1>No connection or app is offline.</h1>;
+}
