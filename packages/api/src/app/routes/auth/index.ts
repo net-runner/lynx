3c1d2a59b063a7f81dcf8de2ad8222bfc550bc60
net-runner <slazynski.j@gmail.com>
2022-05-15 15:54:36 +0200

    First step in using Github Oauth

--- packages/api/src/app/routes/auth/index.ts
+++ packages/api/src/app/routes/auth/index.ts
@@ -2,12 +2,22 @@ import { Router } from 'hyper-express';
 
 const authRouter = new Router();
 
-authRouter.post('/login', async (req, res) => {
+authRouter.post('/signin', async (req, res) => {
   return 'amogus';
 });
 
-authRouter.post('/register', async (req, res) => {
+authRouter.post('/singup', async (req, res) => {
   return 'amogus';
+});
+
+authRouter.get('/signin/github', async (req, res) => {
+  res.redirect('https://github.com/login/oauth/authorize');
+});
+authRouter.post('/signin/github/callback', async (req, res) => {
+  res.redirect('https://github.com/login/oauth/authorize');
+});
+authRouter.get('/signin/google', async (req, res) => {
+  res.redirect('https://github.com/login/oauth/authorize');
 });
 
 export default authRouter;
