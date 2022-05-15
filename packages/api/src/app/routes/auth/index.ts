4ec5376407f3e4d225b17cfb0a2c07734d61fd44
net-runner <slazynski.j@gmail.com>
2022-05-15 12:37:56 +0200

    Added new packages.

--- packages/api/src/app/routes/auth/index.ts
+++ packages/api/src/app/routes/auth/index.ts
@@ -2,4 +2,12 @@ import { Router } from 'hyper-express';
 
 const authRouter = new Router();
 
+authRouter.post('/login', async (req, res) => {
+  return 'amogus';
+});
+
+authRouter.post('/register', async (req, res) => {
+  return 'amogus';
+});
+
 export default authRouter;
