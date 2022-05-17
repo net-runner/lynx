afea74601c87d15934cf47c67c101f88a708a739
net-runner <slazynski.j@gmail.com>
2022-05-17 23:34:52 +0200

    Google auth wip.

--- packages/api/src/app/routes/auth/google.ts
+++ packages/api/src/app/routes/auth/google.ts
@@ -2,10 +2,24 @@ import { v4 as uuidv4 } from 'uuid';
 import axios from 'axios';
 import { Router } from 'hyper-express';
 import pushDiscordWebhook from '../../helpers/pushDiscordWebhook';
+const { GOOGLE_APP_ID, GOOGLE_APP_SECRET, FRONTEND_URL, API_URL } = process.env;
 
 const googleRouter = new Router();
+console.log(GOOGLE_APP_ID);
+console.log(GOOGLE_APP_SECRET);
 
 googleRouter.get('/', async (req, res) => {
-  res.redirect('https://github.com/login/oauth/authorize');
+  res.redirect(
+    `https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=${GOOGLE_APP_ID}&prompt=consent&scope=https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email&redirect_uri=${API_URL}auth/signin/google/callback`
+  );
+});
+googleRouter.get('/url', (req, res) => {
+  return res.send(
+    `https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=${GOOGLE_APP_ID}&prompt=consent&scope=https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email&redirect_uri=${API_URL}auth/signin/google/callback`
+  );
+});
+
+googleRouter.get('/callback', async (req, res) => {
+  res.send('amogus');
 });
 export default googleRouter;
