7f14bc91b1362f49b14f18f84d06ba4317ca139f
net-runner <slazynski.j@gmail.com>
2022-05-20 22:53:06 +0200

    Created initial route layout for links.

--- packages/api/src/app/routes/link/index.ts
+++ packages/api/src/app/routes/link/index.ts
@@ -5,5 +5,11 @@ import requireUser from '../../middlewares/auth/requireUser';
 const linkRouter = new Router();
 
 linkRouter.post('/add', requireUser, handleLinkAdd);
+linkRouter.post('/edit/:id', requireUser, null);
+linkRouter.post('/del/:id', requireUser, null);
+
+//For getting links no auth required
+linkRouter.get('/', null);
+linkRouter.get('/:id', null);
 
 export default linkRouter;
