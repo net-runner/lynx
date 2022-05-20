5bf2df9f5785e3cdc64854b78645662cdb994960
net-runner <slazynski.j@gmail.com>
2022-05-20 23:03:10 +0200

    Added default route layout for LinkGroup

--- packages/api/src/app/routes/linkgroup/index.ts
+++ packages/api/src/app/routes/linkgroup/index.ts
@@ -1,5 +1,14 @@
 import { Router } from 'hyper-express';
+import requireUser from '../../middlewares/auth/requireUser';
 
 const linkGroupRouter = new Router();
+
+linkGroupRouter.post('/add', requireUser, null);
+linkGroupRouter.post('/edit/:id', requireUser, null);
+linkGroupRouter.post('/del/:id', requireUser, null);
+
+//For getting links no auth required
+linkGroupRouter.get('/', null);
+linkGroupRouter.get('/:id', null);
 
 export default linkGroupRouter;
