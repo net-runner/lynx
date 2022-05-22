1e8c772a4500b34a71eab8899ac20f80b755effb
Przemec <pszczepaniak000@gmail.com>
2022-05-22 11:15:12 +0200

    switched multiple const exports in /controllers/link to default export of LinkController class

--- packages/api/src/app/controllers/link/index.ts
+++ packages/api/src/app/controllers/link/index.ts
@@ -11,11 +11,10 @@ import {
   defaultRouteHandler,
 } from '../../../interfaces';
 
-export const handleLinkAdd: authorizedRouteHandler = async (req, res) => {
-  try {
+class LinkController {
+  protected validateAndDestructure = async (req, res, user) => {
     const body = await req.json();
     const { link, privacyLevel, description, group } = body;
-    const user = res.locals.id.user;
     const lynxLink = {
       link,
       privacyLevel,
@@ -26,70 +25,79 @@ export const handleLinkAdd: authorizedRouteHandler = async (req, res) => {
     log.info(link);
 
     const isLinkValidated = await validateLink(lynxLink);
-    if (!isLinkValidated) return res.status(400).end();
+    if (!isLinkValidated) return false;
+    return lynxLink;
+  };
+  public add: authorizedRouteHandler = async (req, res) => {
+    try {
+      const user = res.locals.id.user;
+      const lynxLink = await this.validateAndDestructure(req, res, user);
+      if (!lynxLink) return res.status(400).end();
 
-    await createLink(lynxLink);
+      await createLink(lynxLink);
 
-    const discordWebhookBody = {
-      title: `New link added: ${lynxLink.link}`,
-      description: `from user ${user}`,
-    };
-    pushDiscordWebhook(discordWebhookBody);
-    res.status(200).end();
-  } catch (e) {
-    log.error({ err: e.message, desc: e });
-    res.json({ err: e.message, desc: e });
-  }
-};
+      const discordWebhookBody = {
+        title: `New link added: ${lynxLink.link}`,
+        description: `from user ${user}`,
+      };
+      pushDiscordWebhook(discordWebhookBody);
 
-export const handleGetLink: defaultRouteHandler = async (req, res) => {
-  try {
-    const body = await req.json();
-    const { id } = body;
-    log.info(id);
+      res.status(200).end();
+    } catch (e) {
+      log.error({ err: e.message, desc: e });
+      res.json({ err: e.message, desc: e });
+    }
+  };
+  public getSingle: defaultRouteHandler = async (req, res) => {
+    try {
+      const body = await req.json();
+      const { id } = body;
+      log.info(id);
 
-    const linkFromDb = await getLinkFromDatabase(id);
-    if (!linkFromDb) return res.status(404).end();
+      const linkFromDb = await getLinkFromDatabase(id);
+      if (!linkFromDb) return res.status(404).end();
 
-    const discordWebhookBody = {
-      title: `GET link from db: ${linkFromDb.link}`,
-      description: `link id: ${id}`,
-    };
-    pushDiscordWebhook(discordWebhookBody);
+      const discordWebhookBody = {
+        title: `GET link from db: ${linkFromDb.link}`,
+        description: `link id: ${id}`,
+      };
+      pushDiscordWebhook(discordWebhookBody);
 
-    const linkResponse = JSON.stringify(linkFromDb);
-    res.status(200).send(linkResponse);
-  } catch (e) {
-    log.error({ err: e.message, desc: e });
-    res.json({ err: e.message, desc: e });
-  }
-};
+      const linkResponse = JSON.stringify(linkFromDb);
+      res.status(200).send(linkResponse);
+    } catch (e) {
+      log.error({ err: e.message, desc: e });
+      res.json({ err: e.message, desc: e });
+    }
+  };
+  public getMany: defaultRouteHandler = async (req, res) => {
+    try {
+      const body = await req.json();
+      const { limit } = body;
+      let { page } = body;
 
-export const handleGetLinks: defaultRouteHandler = async (req, res) => {
-  try {
-    const body = await req.json();
-    const { limit } = body;
-    let { page } = body;
+      if (limit > 50) return res.status(400).send('Limit exceeded');
+      if (page === undefined) page = 0;
+      if (typeof limit !== 'number' || typeof page !== 'number')
+        return res.status(400).send('Limit and page have to be numbers');
 
-    if (limit > 50) return res.status(400).send('Limit exceeded');
-    if (page === undefined) page = 0;
-    if (typeof limit !== 'number' || typeof page !== 'number')
-      return res.status(400).send('Limit and page have to be numbers');
+      const linksFromDb = await getLinksFromDatabase(limit, page);
 
-    const linksFromDb = await getLinksFromDatabase(limit, page);
+      const discordWebhookBody = {
+        title: `GET links array from db, limit: ${limit}, page: ${page}`,
+        description: `---`,
+      };
+      pushDiscordWebhook(discordWebhookBody);
 
-    const discordWebhookBody = {
-      title: `GET links array from db, limit: ${limit}, page: ${page}`,
-      description: `---`,
-    };
-    pushDiscordWebhook(discordWebhookBody);
+      const linksResponse = JSON.stringify(
+        linksFromDb.map((linkFromDb) => linkFromDb)
+      );
+      res.status(200).send(linksResponse);
+    } catch (e) {
+      log.error({ err: e.message, desc: e });
+      res.json({ err: e.message, desc: e });
+    }
+  };
+}
 
-    const linksResponse = JSON.stringify(
-      linksFromDb.map((linkFromDb) => linkFromDb)
-    );
-    res.status(200).send(linksResponse);
-  } catch (e) {
-    log.error({ err: e.message, desc: e });
-    res.json({ err: e.message, desc: e });
-  }
-};
+export default new LinkController();
--- packages/api/src/app/routes/link/index.ts
+++ packages/api/src/app/routes/link/index.ts
@@ -1,17 +1,17 @@
 import { Router } from 'hyper-express';
-import {handleGetLink, handleGetLinks, handleLinkAdd} from '../../controllers/link';
+import LinkController from '../../controllers/link';
 import requireUser from '../../middlewares/auth/requireUser';
 
 const linkRouter = new Router();
 
-linkRouter.post('/add', requireUser, handleLinkAdd);
+linkRouter.post('/add', requireUser, LinkController.add);
 linkRouter.post('/edit/:id', requireUser, null);
 linkRouter.post('/del/:id', requireUser, null);
 
 //For getting links no auth required
 //implement pagination for /
 linkRouter.get('/', null);
-linkRouter.get('/:id', handleGetLink);
-linkRouter.get('/:limit/:page', handleGetLinks);
+linkRouter.get('/:id', LinkController.getSingle);
+linkRouter.get('/:limit/:page', LinkController.getMany);
 
 export default linkRouter;
