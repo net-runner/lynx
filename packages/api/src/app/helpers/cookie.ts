eb4a0539d7d21f18c314d35bbd75e14f738a78af
net-runner <slazynski.j@gmail.com>
2022-05-23 18:26:08 +0200

    Cookie settings updated.

--- packages/api/src/app/helpers/cookie.ts
+++ packages/api/src/app/helpers/cookie.ts
@@ -3,13 +3,15 @@ import { CookieOptions } from 'hyper-express';
 const { FRONTEND_URL } = process.env;
 const env = process.env.NODE_ENV;
 
+const isProduction = env === 'production';
+
 export const cookieOptions: CookieOptions = {
-  maxAge: 900000,
+  maxAge: 15 * 60 * 1000,
   httpOnly: true,
   domain: FRONTEND_URL,
   path: '/',
-  sameSite: 'strict',
-  secure: false,
+  sameSite: isProduction ? 'strict' : 'lax',
+  secure: isProduction,
 };
 
 export const refreshCookieOptions: CookieOptions = {
