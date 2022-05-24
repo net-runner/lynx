0d546154472c86c41cda6b36fde2ebb322cec84a
net-runner <slazynski.j@gmail.com>
2022-05-24 21:30:57 +0200

    Not *

--- packages/api/src/app/middlewares/cors/index.ts
+++ packages/api/src/app/middlewares/cors/index.ts
@@ -7,8 +7,7 @@ const isProduction = env === 'production';
 const corsMiddleware: defaultRouteMiddlewareInterface = (req, res, next) => {
   res.header(
     'Access-Control-Allow-Origin',
-    // `${isProduction ? FRONTEND_URL : '*'}`
-    `*`
+    `${isProduction ? FRONTEND_URL : '*'}`
   );
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header(
