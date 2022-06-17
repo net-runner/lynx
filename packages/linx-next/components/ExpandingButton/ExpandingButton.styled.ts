0ec0ad0936a3039626a101c7ebc288effa6109ac
net-runner <slazynski.j@gmail.com>
2022-06-17 14:35:47 +0200

    chore: Removed unused styles.

--- packages/linx-next/components/ExpandingButton/ExpandingButton.styled.ts
+++ packages/linx-next/components/ExpandingButton/ExpandingButton.styled.ts
@@ -19,31 +19,6 @@ export const Wrapper = styled.div`
   }
 `;
 
-export const Form = styled.form`
-  padding: 2rem;
-  border-radius: 2rem;
-  background-color: ${({ theme }) => theme.backgroundSecondary};
-`;
-
-export const Input = styled.input`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 30rem;
-  height: 4rem;
-  margin: 0.4rem 0 1.2rem;
-  padding: 0.5rem 1rem;
-  border-color: rgba(249, 249, 249, 0.25);
-  border-width: 0.1rem;
-  border-radius: 1rem;
-  background: transparent;
-  color: #f9f9f9;
-  font-weight: bold;
-  font-size: 1.4rem;
-  font-family: 'Poppins', sans-serif;
-  white-space: nowrap;
-`;
-
 export const IconWrapper = styled.div`
   cursor: pointer;
   z-index: 11;
