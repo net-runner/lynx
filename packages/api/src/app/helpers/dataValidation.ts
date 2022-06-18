18223cb64b61ffe3819e08f21c187d95ec9c1beb
Przemec <pszczepaniak000@gmail.com>
2022-06-18 20:56:05 +0200

    chore: Updated links validation

--- packages/api/src/app/helpers/dataValidation.ts
+++ packages/api/src/app/helpers/dataValidation.ts
@@ -30,7 +30,16 @@ export const validateSignUp = async (user: LynxUser): Promise<boolean> => {
 
 const linkSchema = Joi.object({
   id: Joi.string().guid(),
-  link: Joi.string().uri(),
+  link: Joi.string()
+    .uri()
+    .custom((value) => {
+      const firstDoubleSlashIndex = value.indexOf('//');
+      const lastDoubleSlashIndex = value.lastIndexOf('//');
+      if (firstDoubleSlashIndex !== lastDoubleSlashIndex)
+        throw new Error(
+          'repeated forward-slashes (//) are not valid in the link'
+        );
+    }),
   description: Joi.string().min(5),
   privacyLevel: Joi.number(),
   owner: Joi.string().guid(),
@@ -51,7 +60,6 @@ export const validateLink = async (
     value = linkAddSchema.validate(link);
   if (actionType === ControllerMethodTypes.EDIT)
     value = linkSchema.validate(link);
-  console.log(value.error)
   return !value.error;
 };
 
