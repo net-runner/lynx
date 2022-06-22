ff3dff0bc4ab2f2b04789e17c3ad73f9fd6ff6c6
Przemec <pszczepaniak000@gmail.com>
2022-06-22 19:00:18 +0200

    chore: Refactored assets/icons exports

--- packages/linx-next/assets/icons/index.ts
+++ packages/linx-next/assets/icons/index.ts
@@ -1,37 +1,18 @@
-import GithubIcon from './GithubIcon';
-import GithubOutlineIcon from './GithubOutlineIcon';
-import LinkIcon from './LinkIcon';
-import GoogleIcon from './GoogleIcon';
-import LogoSmall from './LogoSmall';
-import LynxLogoDetail from './LynxLogoDetail';
-import LynxLogoDetailNoCircle from './LynxLogoDetailNoCircleSmallBox';
-import LynxLogoDetailNoCircleSmallBox from './LynxLogoDetailNoCircleSmallBox';
-import NoConnectionIcon from './NoConnectionIcon';
-import ChevronDown from './ChevronDown';
-import LinkedAmountIcon from './LinkedAmountIcon';
-import WatchersIcon from './WatchersIcon';
-import StarEmpty from './StarEmpty';
-import StarHalf from './StarHalf';
-import StarFull from './StarFull';
-import OpenInNewTab from './OpenInNewTab';
-import AddIcon from './AddIcon';
-
-export {
-  ChevronDown,
-  GithubIcon,
-  GithubOutlineIcon,
-  LinkIcon,
-  GoogleIcon,
-  LogoSmall,
-  LynxLogoDetail,
-  LynxLogoDetailNoCircle,
-  NoConnectionIcon,
-  LynxLogoDetailNoCircleSmallBox,
-  LinkedAmountIcon,
-  WatchersIcon,
-  StarEmpty,
-  StarHalf,
-  StarFull,
-  OpenInNewTab,
-  AddIcon,
-};
+export { default as GithubIcon } from './GithubIcon';
+export { default as GithubOutlineIcon } from './GithubOutlineIcon';
+export { default as LinkIcon } from './LinkIcon';
+export { default as GoogleIcon } from './GoogleIcon';
+export { default as LogoSmall } from './LogoSmall';
+export { default as LynxLogoDetail } from './LynxLogoDetail';
+export { default as LynxLogoDetailNoCircle } from './LynxLogoDetailNoCircleSmallBox';
+export { default as LynxLogoDetailNoCircleSmallBox } from './LynxLogoDetailNoCircleSmallBox';
+export { default as NoConnectionIcon } from './NoConnectionIcon';
+export { default as ChevronDown } from './ChevronDown';
+export { default as LinkedAmountIcon } from './LinkedAmountIcon';
+export { default as WatchersIcon } from './WatchersIcon';
+export { default as StarEmpty } from './StarEmpty';
+export { default as StarHalf } from './StarHalf';
+export { default as StarFull } from './StarFull';
+export { default as OpenInNewTab } from './OpenInNewTab';
+export { default as AddIcon } from './AddIcon';
+export { default as Lock } from './Lock';
