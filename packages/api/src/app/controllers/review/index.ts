9d33d81b7d8aedc7e77fa0ccd426dff5ba789f67
net-runner <slazynski.j@gmail.com>
2022-06-24 21:50:31 +0200

    feat: Gated review add/delete behind authorization.

--- packages/api/src/app/controllers/review/index.ts
+++ packages/api/src/app/controllers/review/index.ts
@@ -3,20 +3,28 @@ import { authorizedRouteHandler } from '../../../interfaces';
 import log from '../../helpers/logger';
 import { pushDiscordWebhook } from '../../helpers/pushDiscordWebhook';
 import { createReview, deleteReview } from '../../services/review';
+import { getUserById } from '../../services/user';
 const handleReviewAdd: authorizedRouteHandler = async (req, res) => {
   try {
     const body = (await req.json()) as Omit<Review, 'id' | 'link'>;
 
-    const review = await createReview(body);
-    if (!review) return res.status(404).end();
+    //Check user
+    const usrId = res.locals.id.user;
+    const user = await getUserById(usrId);
+    if (user && body.creatorName === user.username) {
+      const review = await createReview(body);
+      if (!review) return res.status(404).end();
 
-    const discordWebhookBody = {
-      title: `Created new review | creatorName: ${body.creatorName}`,
-      description: `with score: ${body.score} | description: ${body.description}`,
-    };
-    pushDiscordWebhook(discordWebhookBody);
+      const discordWebhookBody = {
+        title: `Created new review | creatorName: ${body.creatorName}`,
+        description: `with score: ${body.score} | description: ${body.description}`,
+      };
+      pushDiscordWebhook(discordWebhookBody);
 
-    res.status(200).json({ success: true });
+      res.status(200).json({ success: true });
+    } else {
+      return res.status(403).end();
+    }
   } catch (e) {
     log.error({ err: e.message, desc: e });
     return res.status(500).json({ err: e.message, desc: e });
@@ -25,8 +33,10 @@ const handleReviewAdd: authorizedRouteHandler = async (req, res) => {
 const handleReviewDelete: authorizedRouteHandler = async (req, res) => {
   try {
     const id = req.params.id;
+    const usrId = res.locals.id.user;
+    const user = await getUserById(usrId);
 
-    const review = await deleteReview(id);
+    const review = await deleteReview(id, user.username);
     if (!review) return res.status(404).end();
 
     const discordWebhookBody = {
--- packages/api/src/app/services/review.ts
+++ packages/api/src/app/services/review.ts
@@ -1,9 +1,24 @@
 import { Review } from '@prisma/client';
+import { deleteFromCache, getFromCache, setExCache } from '../helpers/redis';
 import db from '../lib/db';
 
 export async function createReview(review: Omit<Review, 'id' | 'link'>) {
-  return await db.review.create({ data: review });
+  const r = await db.review.create({ data: review });
+  setExCache(r.id, 36000, JSON.stringify(r));
+  return r;
 }
-export async function deleteReview(id: string) {
-  return await db.review.delete({ where: { id } });
+export async function deleteReview(id: string, creatorName: string) {
+  const cachedReview = await getFromCache(id);
+
+  let rev;
+  if (cachedReview) {
+    rev = cachedReview;
+  } else {
+    rev = await db.review.findUnique({ where: { id } });
+  }
+
+  if (rev && rev.creatorName === creatorName) {
+    deleteFromCache(id);
+    return await db.review.delete({ where: { id } });
+  }
 }
