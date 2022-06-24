388e3fa7a051ac5a8d3ca6060e750c3da7f4ae62
Przemec <pszczepaniak000@gmail.com>
2022-06-24 21:37:34 +0200

    feat: Added option to remove link from linkgroup

--- packages/linx-next/components/LinkComponent/LinkComponent.styled.ts
+++ packages/linx-next/components/LinkComponent/LinkComponent.styled.ts
@@ -2,33 +2,52 @@ import styled from 'styled-components';
 import { LinkIcon } from '../../assets/icons';
 
 export const NewTabIconWrapper = styled.a`
-  display: none;
+  cursor: pointer;
+  display: flex;
   align-items: center;
   justify-content: center;
+  visibility: hidden;
+  margin-left: 1rem;
 
   svg {
     width: 2.5rem;
     height: 2.5rem;
-    margin-left: 1rem;
   }
 `;
 
-export const LContainer = styled.div`
+export const DeleteIconWrapper = styled.div`
   cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  visibility: hidden;
+  margin-left: 0.8rem;
+`;
+
+export const LContainer = styled.div`
   width: 100%;
   padding-top: 1rem;
   display: flex;
   align-items: center;
   justify-content: flex-start;
 
-  &:hover ${NewTabIconWrapper} {
-    display: flex;
+  span {
+    cursor: pointer;
+  }
+
+  svg {
+    width: 2.5rem;
+    height: 2.5rem;
+  }
+
+  &:hover {
+    ${DeleteIconWrapper}, ${NewTabIconWrapper} {
+      visibility: visible;
+    }
   }
 `;
 
 export const LinkIco = styled(LinkIcon)`
   transform: rotate(-45deg);
-  width: 2.5rem;
-  height: 2.5rem;
   margin-right: 1rem;
 `;
